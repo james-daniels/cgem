20692236e9837597807f60a4e0fd459f8db09fe8
James Daniels <james.daniels@nokia.com>
2022-05-08 00:33:18 -0500

    added pretty output for OneInst function

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module james-daniels/gemini
 
 go 1.18
 
-require gopkg.in/ini.v1 v1.66.4 // indirect
+require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+)
--- recurrence.go
+++ recurrence.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
+
+	"gopkg.in/ini.v1"
 )
+
+var (
+	pretty bool
+)
+
+func init() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	pretty, err = cfg.Section("").Key("pretty").Bool()
+	if err != nil {
+		log.Fatalf("Failed to read parameter: %v", err)
+	}
+}
 
 func OneInst(baseurl string) {
 
@@ -18,7 +37,11 @@ func OneInst(baseurl string) {
 	response, err := newOrder(baseurl, payload, signature)
 	errHandler(err)
 
-	log.Printf("%+v\n", response)
+	if pretty {
+		fmt.Println(Pretty(response))
+	} else {
+		fmt.Printf("%+v\n", response)
+	}
 }
 
 func MultiInst(baseurl string, freq int) {
@@ -51,4 +74,33 @@ func errHandler(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func Pretty(r Response) string {
+	resp := fmt.Sprintf(`
+	OrderID:		%v
+	ID:			%v
+	Symbol:			%v
+	Exchange:		%v
+	AvgExecutionPrice:	%v
+	Side:			%v
+	Type:			%v
+	Timestamp:		%v
+	Timestampms:		%v
+	IsLive:			%v
+	IsCancelled:		%v
+	IsHidden:		%v
+	WasForced:		%v
+	ExecutedAmount:		%v
+	Options:		%v
+	StopPrice:		%v
+	Price:			%v
+	OriginalAmount:		%v
+	`, r.OrderID, r.ID, r.Symbol, r.Exchange,
+		r.AvgExecutionPrice, r.Side, r.Type, r.Timestamp,
+		r.Timestampms, r.IsLive, r.IsCancelled, r.IsHidden,
+		r.WasForced, r.ExecutedAmount, r.Options, r.StopPrice,
+		r.Price, r.OriginalAmount)
+
+	return resp
 }
